21aa3158b2489a4bf6d0422e6e4bf381bfbdf7da
Rujool Doshi <rujool@gmail.com>
2017-10-30 23:34:40 -0500

    Test code for Public Key generation

--- src/AuthenticateServer.java
+++ src/AuthenticateServer.java
@@ -8,7 +8,9 @@ import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.Provider;
 import java.security.PublicKey;
+import java.security.Security;
 import java.security.SignatureException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -17,17 +19,35 @@ import java.security.spec.KeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
+
 public class AuthenticateServer {
 	public static void authServer(X509Certificate cert) {
 		try {
-			FileInputStream keyfis = new FileInputStream("/home/dell/ashkan-public.key");
-			String publicKeyStr = new String(Files.readAllBytes(Paths.get("/home/dell/ashkan-public.key")));
-			keyfis.close();
-			KeyFactory kf = KeyFactory.getInstance("RSA");
-			PublicKey pk = kf.generatePublic(spec);
-			cert.verify(pk);
 			
-		} catch (IOException e) {
+			String keyStr = new String(Files.readAllBytes(Paths.get("/home/dell/ashkan-public.key")));
+			System.out.println("KeyStr: "+keyStr);
+			System.out.println("keyStr length: " + keyStr.length());
+			String publicKeyStr = //"-----BEGIN PUBLIC KEY-----"
+					  "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtIOcQbyOkWkT/1N4mI7m"
+					+ "6wmv7J1yNiakGSr4LOV7WAZpAO0jOPUDQTKn6UEDcYIlaMbIvzKebtFKx59MDNLQ"
+					+ "OyNBrm6U38Hlr3jUYsUoP0DqWSRBSdeV5eEpvgioNWr1yEhpxPjHaEvQvgbQ8y1a"
+					+ "sUjIGJuRR69W6JcrYnwPvZ6mco8N9qBUw4IoiHiNxUCo5XKhZIJF/69Dm+FkndS4"
+					+ "xCo6gQ24U5zSabUIHeWnfGn5OUtYwHnysvUO1RyHdHbbgnCThP/5kF0EV8AffHra"
+					+ "c5M6Otyd1bzDB/ldX75VXb8Bq6JraSHsDOsKWgplCEWJcT1xlDRCvfgWGhTU3AOa"
+					+ "QwIDAQAB";
+								// + "-----END PUBLIC KEY-----"
+			System.out.println(publicKeyStr.length());
+			KeySpec spec =  new X509EncodedKeySpec(Base64.getDecoder().decode(keyStr));
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			PublicKey publicKey = kf.generatePublic(spec);
+			
+			/*X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKeyStr));
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			PublicKey publicKey = kf.generatePublic(pubKeySpec);*/
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		/*catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
@@ -36,19 +56,7 @@ public class AuthenticateServer {
 		} catch (InvalidKeySpecException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (CertificateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchProviderException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SignatureException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
+		}	*/
 		
 	}
 }
